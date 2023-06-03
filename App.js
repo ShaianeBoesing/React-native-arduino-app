dca3b573147cd31596853d08cd398353fdd877dd
Shaiane Boesing Rodrigues Borges <shaianeboesingrb@gmail.com>
2023-06-02 22:30:27 -0300

    mensagem

--- App.js
+++ App.js
@@ -1,20 +1,121 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TextInput } from 'react-native';
+import ButtonWithLoader from './components/ButtonWithLoader';
 
-export default function App() {
+const App = () => {
+  const [waitingForSetState, setWaitingForSetState] = useState(false);
+  const [waitingForSetIntensity, setWaitingForSetIntensity] = useState(false);
+  const [intensity, setIntensity] = useState('0');
+  const [state, setState] = useState('off');
+
+  const onPressSetState = async () => {
+    setWaitingForSetState(true);
+    let newState = state == 'on' ? 'off' : 'on';
+    console.log(newState);
+    fetch(`http://localhost:80/setState/${newState}`)
+      .then(response => response.text())
+      .then(data => {
+        setState(newState);
+        console.log(data);
+        alert(data);
+      })
+      .catch(error => {
+        console.error('Erro:', error);
+        alert(error);
+      })
+      .finally(() => {
+        setWaitingForSetState(false);
+      });
+  };
+
+  const onPressSetIntensityLed = () => {
+    setWaitingForSetIntensity(true);
+    let newIntensity = intensity == '' ? -1 : intensity; 
+    fetch(`http://localhost:80/intensity/${newIntensity }`)
+      .then(response => response.text())
+      .then(data => {
+        console.log(data);
+        alert(data);
+      })
+      .catch(error => {
+        console.error('Erro:', error);
+        alert(error);
+      })
+      .finally(() => {
+        setWaitingForSetIntensity(false);
+      });
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={styles.container}>
+        
+        <Text style={styles.title}>Mudar estado do LED</Text>
+        <ButtonWithLoader
+          title="Led ON/OFF"
+          onPress={onPressSetState}
+          isLoading={waitingForSetState}
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Digite a intensidade"
+          onChangeText={text => setIntensity(text)}
+          value={intensity}
+          keyboardType="numeric"
+        />
+        <ButtonWithLoader
+          title="Mudar Intensidade"
+          onPress={onPressSetIntensityLed}
+          isLoading={waitingForSetIntensity}
+        />
+      </View>
+    </SafeAreaView>
   );
-}
+  };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#fff0c7',
   },
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#8a5336',
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderRadius: 4,
+  },
+  buttonLoader: {
+    marginLeft: 8,
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 16,
+  },
+  title: {
+    marginBottom: 10,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
+  },
+  input: {
+    width: 200,
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 4,
+    paddingHorizontal: 10,
+    marginRight: 10,
+  },
+
 });
+
+export default App;
---
+++ components/ButtonWithLoader.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity, ActivityIndicator } from 'react-native';
+
+
+
+const ButtonWithLoader = ({ title, onPress, isLoading }) => {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={styles.button}
+      disabled={isLoading}
+    >
+      {isLoading && (
+        <ActivityIndicator color="#ffffff" style={styles.buttonLoader} />
+      )}
+      <Text style={styles.buttonText}>{isLoading? "Processando" : title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#8a5336',
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    borderRadius: 4,
+  },
+  buttonLoader: {
+    marginRight: 8,
+  },
+  buttonText: {
+    color: '#ffffff',
+    fontSize: 16,
+  },
+});
+
+export default ButtonWithLoader;
